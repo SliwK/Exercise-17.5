cda8a5fcf9c6cfb9f0578d791b01a0ae383a3942
Krzysztof Sliwinski <kpsliwinski@gmail.com>
2018-02-18 16:20:02 +0100

    Correct js

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var app = express();
 
 app.use("/store", function(req, res, next){
     console.log('Hej, jestem pośrednikiem przy żądaniu do /store.');
+    res.on('finish', function() {
+      console.log('Wysłałem odpowiedź!') ;
+    });
     next();
 });
 
@@ -10,12 +13,8 @@ app.get('/', function (req, res) {
   res.send("hello world!");
 });
 
-app.get("/store", function (req, res,next) {
+app.get("/store", function (req, res) {
   res.send("To jest sklep");
-  res.on('finish', function() {
-    console.log('Wysłałem odpowiedź!') ;
-  });
-  next();
 });
 
 var server = app.listen(3000, 'localhost', function() {
