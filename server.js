d4bcf66c5f9d96f4090fb193fb6e77f4754dbaa4
Krzysztof Sliwinski <kpsliwinski@gmail.com>
2018-02-18 16:08:16 +0100

    Correct js

--- server.js
+++ server.js
@@ -10,10 +10,13 @@ app.get('/', function (req, res) {
   res.send("hello world!");
 });
 
-app.get("/store", function (req, res) {
+app.get("/store", function (req, res,next) {
   res.send("To jest sklep");
+  res.on('finish', function() {
+    console.log('Wysłałem odpowiedź!') ;
+  });
+  next();
 });
-
 
 var server = app.listen(3000, 'localhost', function() {
     console.log('Przykładowa aplikacja nasłuchuje');
