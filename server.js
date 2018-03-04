586503f7dfbfd2c1520b21470699a96f887d20e5
Krzysztof Sliwinski <kpsliwinski@gmail.com>
2018-03-04 06:21:44 +0100

    Corrected middleware

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ var app = express();
 
 app.use("/store", function(req, res, next){
     console.log('Hej, jestem pośrednikiem przy żądaniu do /store.');
-    res.on('finish', function() {
+/*    res.on('finish', function() {
       console.log('Wysłałem odpowiedź!') ;
     });
+    */  //to bardziej wynika z NODA - https://nodejs.org/api/http.html#http_event_finish
     next();
 });
 
@@ -13,8 +14,12 @@ app.get('/', function (req, res) {
   res.send("hello world!");
 });
 
-app.get("/store", function (req, res) {
+app.get("/store", function (req, res,next) {
   res.send("To jest sklep");
+  next(); //next wywołuje kolejną funkcję w tym łańcuchu wywołań - tak to powinno być zaimplementowane
+  res.on('finish', function() {
+    console.log('Wysłałem odpowiedź!') ;
+  });
 });
 
 var server = app.listen(3000, 'localhost', function() {
